5f5124f8a64178deb86e3c95b6618bb308d69cfe
Linh Truong Cong Hong <linhtch90@gmail.com>
2020-08-11 18:24:39 +0700

    Modify with changing background photo

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     :class="
       typeof weather.main != 'undefined' && weather.main.temp > 25 ? 'warm' : ''
     "
+    v-bind:style="{ 'background-image': 'url(' + groundPhoto + ')' }"
   >
     <main>
       <div class="search-box">
@@ -12,7 +13,10 @@
           class="search-bar"
           placeholder="Enter city name..."
           v-model="query"
-          v-on:keypress="fetchWeather"
+          v-on:keyup.enter="
+            fetchWeather();
+            setGroundPhoto();
+          "
         />
       </div>
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
@@ -24,7 +28,7 @@
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }} &#176;C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
@@ -38,22 +42,25 @@ export default {
   data: function() {
     return {
       api_key: "286ffa706125a2024a8f265d1747310f",
+      unsplashId: "X--qz7h6H6I8iON27rFSFCk_uTx-EcBfwpJr07-YU6o",
+
       url_base: "https://api.openweathermap.org/data/2.5/",
+      urlUnsplash: "https://api.unsplash.com/search/photos/?client_id=",
       query: "",
       weather: {},
+      groundPhoto:
+        "https://images.unsplash.com/photo-1566996694954-90b052c413c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     };
   },
   methods: {
-    fetchWeather(e) {
-      if (e.key == "Enter") {
-        fetch(
-          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .then(this.setResults);
-      }
+    fetchWeather() {
+      fetch(
+        `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then(this.setResults);
     },
     setResults(results) {
       this.weather = results;
@@ -70,11 +77,24 @@ export default {
 
       return date;
     },
+    setGroundPhoto() {
+      fetch(`${this.urlUnsplash}${this.unsplashId}&query=${this.query}`)
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          console.log(data);
+          this.groundPhoto = data.results[0].urls.regular;
+          console.log(this.groundPhoto);
+        });
+    },
   },
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap");
+
 * {
   margin: 0;
   padding: 0;
@@ -82,18 +102,20 @@ export default {
 }
 
 body {
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: "Rubik", sans-serif;
 }
 
 #app {
-  background-image: url("./assets/cold-bg.jpg");
+  /* background-image: url("./assets/cold-bg.jpg"); */
+  background-image: url("https://images.unsplash.com/photo-1566996694954-90b052c413c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80");
+
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
 
 #app.warm {
-  background-image: url("./assets/warm-bg.jpg");
+  /* background-image: url("./assets/warm-bg.jpg"); */
 }
 
 main {
@@ -107,16 +129,16 @@ main {
 }
 
 .search-box {
-  width: 100%;
-  margin-bottom: 30px;
+  width: 60%;
+  margin: 40px auto;
 }
 
 .search-box .search-bar {
   display: block;
   width: 100%;
-  padding: 15px;
+  padding: 20px;
   color: #313131;
-  font-size: 20px;
+  font-size: 32px;
   appearance: none;
   border: none;
   outline: none;
@@ -135,8 +157,8 @@ main {
 
 .location-box .location {
   color: white;
-  font-size: 46px;
-  font-weight: 500px;
+  font-size: 50px;
+  font-weight: 500;
   text-align: center;
   text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
 }
@@ -144,7 +166,7 @@ main {
 .location-box .date {
   color: rgba(255, 255, 255, 0.75);
   font-size: 30px;
-  font-weight: 300;
+  font-weight: 400;
   font-style: italic;
   text-align: center;
   margin-top: 10px;
